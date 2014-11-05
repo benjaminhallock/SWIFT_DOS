ae22714aacc35b9da125f75bf3e6e58a8c339f6f
benben <benben>
2014-11-05 14:04:24 -0600

    Initial Commit

---
+++ SWIFT 2Tests/SWIFT_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  SWIFT_2Tests.swift
+//  SWIFT 2Tests
+//
+//  Created by benjaminhallock@gmail.com on 11/5/14.
+//  Copyright (c) 2014 BEN. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SWIFT_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
